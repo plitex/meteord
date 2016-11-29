550b40dd0827f5528da5cf179b7b1d64858c554f
Miguel Mejias <plitex@gmail.com>
2016-11-29 23:56:31 +0100

    Disable binary and bundle tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -9,9 +9,10 @@ bash ./test_meteor_app_with_devbuild.sh
 # I don't believe it's possible for this test to work right now.
 # bash ./test_bundle_local_mount.sh
 
-bash ./test_bundle_web.sh
-bash ./test_binary_build_on_base.sh
-bash ./test_binary_build_on_bin_build.sh
+# We need to finish these tests
+#bash ./test_bundle_web.sh
+#bash ./test_binary_build_on_base.sh
+#bash ./test_binary_build_on_bin_build.sh
 
 bash ./test_phantomjs.sh
 bash ./test_no_app.sh
