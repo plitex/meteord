34977f67d948db402cb06eb323dbc84fb711b4a1
Miguel Mejias <plitex@gmail.com>
2016-11-29 23:28:17 +0100

    Download PhantomJS from googleapis and downgrade to 1.9.2

--- base/scripts/lib/install_phantomjs.sh
+++ base/scripts/lib/install_phantomjs.sh
@@ -1,12 +1,14 @@
 # Install PhantomJS
 set -e
-apt-get -y install libfreetype6 libfreetype6-dev fontconfig
+
+apt-get install -y libfreetype6 libfreetype6-dev fontconfig
+
 ARCH=`uname -m`
-PHANTOMJS_VERSION=1.9.8
+PHANTOMJS_VERSION=1.9.2
 PHANTOMJS_TAR_FILE=phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}.tar.bz2
 
 cd /usr/local/share/
-curl -L -O https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}.tar.bz2
+curl -L -O https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/phantomjs/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}.tar.bz2
 tar xjf $PHANTOMJS_TAR_FILE
 ln -s -f /usr/local/share/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}/bin/phantomjs /usr/local/share/phantomjs
 ln -s -f /usr/local/share/phantomjs-${PHANTOMJS_VERSION}-linux-${ARCH}/bin/phantomjs /usr/local/bin/phantomjs
